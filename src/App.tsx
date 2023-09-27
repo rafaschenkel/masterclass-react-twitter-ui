593f2a2805eec8f20989e5f33b40d4ea06d70aa0
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-09-27 17:40:53 -0300

    create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,62 @@
+import {
+  Bell,
+  Envelope,
+  Hash,
+  House,
+  BookmarkSimple,
+  FileText,
+  User,
+  DotsThreeCircle
+} from '@phosphor-icons/react';
+
+import twitterLogo from '../public/twitter-logo.svg';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="layout">
+      <aside className="sideBar">
+        <header className="sideBar-header">
+          <img src={twitterLogo} alt="" />
+        </header>
+        <div className="sideBar-links">
+          <a href="" className="sideBar-link active">
+            <House weight="fill" size={30} />
+            Home
+          </a>
+          <a href="" className="sideBar-link">
+            <Hash size={30} />
+            Explorer
+          </a>
+          <a href="" className="sideBar-link">
+            <Bell size={30} />
+            Notifications
+          </a>
+          <a href="" className="sideBar-link">
+            <Envelope size={30} />
+            Messages
+          </a>
+          <a href="" className="sideBar-link">
+            <BookmarkSimple size={30} />
+            Bookmarks
+          </a>
+          <a href="" className="sideBar-link">
+            <FileText size={30} />
+            Lists
+          </a>
+          <a href="" className="sideBar-link">
+            <User size={30} />
+            Profile
+          </a>
+          <a href="" className="sideBar-link">
+            <DotsThreeCircle size={30} />
+            More
+          </a>
+        </div>
+        <button>Tweet</button>
+      </aside>
+      <div className="content">k</div>
+    </div>
+  );
 }
 
 export default App;
