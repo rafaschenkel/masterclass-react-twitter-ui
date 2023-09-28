10c4d43ccb766f6e7bfbe98f78fe3719e406b5a9
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-09-28 11:42:16 -0300

    criado component Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,11 @@
-import {
-  Bell,
-  Envelope,
-  Hash,
-  House,
-  BookmarkSimple,
-  FileText,
-  User,
-  DotsThreeCircle,
-  Sparkle
-} from '@phosphor-icons/react';
-
-import twitterLogo from '../public/twitter-logo.svg';
+import { Sparkle } from '@phosphor-icons/react';
 import { Tweet } from './components/Tweet';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <div className="layout">
-      <aside className="sidebar">
-        <img className="logo" src={twitterLogo} alt="Logo Twitter" />
-        <nav className="sidebar-navigation">
-          <a href="" className="active">
-            <House weight="fill" size={30} />
-            Home
-          </a>
-          <a href="">
-            <Hash size={30} />
-            Explorer
-          </a>
-          <a href="">
-            <Bell size={30} />
-            Notifications
-          </a>
-          <a href="">
-            <Envelope size={30} />
-            Messages
-          </a>
-          <a href="">
-            <BookmarkSimple size={30} />
-            Bookmarks
-          </a>
-          <a href="">
-            <FileText size={30} />
-            Lists
-          </a>
-          <a href="">
-            <User size={30} />
-            Profile
-          </a>
-          <a href="">
-            <DotsThreeCircle size={30} />
-            More
-          </a>
-        </nav>
-        <button className="new-tweet">Tweet</button>
-      </aside>
+      <Sidebar />
       <div className="content">
         <main className="timeline">
           <div className="timeline-header">
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,55 @@
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  User
+} from '@phosphor-icons/react';
+import twitterLogo from '../assets/twitter-logo.svg';
+import './Sidebar.css';
+
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <img className="logo" src={twitterLogo} alt="Logo Twitter" />
+      <nav className="sidebar-navigation">
+        <a href="" className="active">
+          <House weight="fill" size={30} />
+          Home
+        </a>
+        <a href="">
+          <Hash size={30} />
+          Explorer
+        </a>
+        <a href="">
+          <Bell size={30} />
+          Notifications
+        </a>
+        <a href="">
+          <Envelope size={30} />
+          Messages
+        </a>
+        <a href="">
+          <BookmarkSimple size={30} />
+          Bookmarks
+        </a>
+        <a href="">
+          <FileText size={30} />
+          Lists
+        </a>
+        <a href="">
+          <User size={30} />
+          Profile
+        </a>
+        <a href="">
+          <DotsThreeCircle size={30} />
+          More
+        </a>
+      </nav>
+      <button className="new-tweet">Tweet</button>
+    </aside>
+  );
+}
